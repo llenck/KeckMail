b8e29790d0f259f5672466d4604bbebe9a1b5829
Lars Lenckowski <larslenckowski@gmail.com>
2020-09-14 21:12:45 +0200

    resolve merge conflict with kek branch

--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@ import config
 from exceptions import QuitException
 from connection_state import ConnectionState
 
+
 def helo_handler(state, command):
     return "250 %s, at your service uwu" % config.domain
 
@@ -21,14 +22,13 @@ def mail_handler(state, command):
 def data_handler(state, command):
     state.multiline_command = b""
     state.next_multiline_handler = b"data_finish"
-    print("set m_command to b\"\" and next_m_handler to %s" % \
-        state.next_multiline_handler)
+    print("set m_command to b\"\" and next_m_handler to %s" %
+          state.next_multiline_handler)
 
     return "354 Gib Daten"
 
 def data_finish_handler(state, line):
     state.mail.data["content"] = line
-
     return "250 ok boomer"
 
 def rctp_handler(state, line):
@@ -36,9 +36,9 @@ def rctp_handler(state, line):
         receiver = line.split(b"<")[1].split(b">")[0]
 
         if state.mail.data["receiver"] != "":
-            print("Warning: client send multiple recipients, discarding" + \
-                "the old one (%s)" % state.mail.data["receiver"])
-    
+            print("Warning: client send multiple recipients, discarding" +
+                  "the old one (%s)" % state.mail.data["receiver"])
+
         state.mail.data["receiver"] = str(receiver, encoding="ascii")
         return "250 OK BOOMER"
 
@@ -55,6 +55,9 @@ def rset_handler(state, line):
     state.mail = Mail()
     return "250 y tho"
 
+def help_handler(state, line):
+    return "250 Du nix kriegen Hilfe"
+
 handlers = {
     b"HELO": helo_handler,
     b"EHLO": ehlo_handler,
@@ -64,6 +67,7 @@ handlers = {
     b"RCPT": rctp_handler,
     b"QUIT": quit_handler,
     b"RSET": rset_handler,
+    b"HELP": help_handler,
 }
 
 def call_handler(state, line, cmd):
