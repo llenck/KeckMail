615cba8393257802c8b331d3f1795ba055ac61f5
bgfxc4 <bindermarc@gmx.de>
2020-09-15 23:13:42 +0200

    made mongodb backend saving async

--- save_mail_mongo.py
+++ save_mail_mongo.py
@@ -1,9 +1,17 @@
 import pymongo
 import config
+import motor.motor_asyncio
+import asyncio
 
-mongo_client = pymongo.MongoClient(mongodb_domain)
-db = myclient["KeckMailDB"]
+motor_client = motor.motor_asyncio.AsyncIOMotorClient(config.mongodb_domain)
+db = motor_client["KeckMailDB"]
 dbcol_mails = db["mails"]
 
-def save_mail(mail):
-    dbcol_mails.insert_one(mail)
+async def save_mail(mail):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(insert_mail(mail))
+
+
+async def insert_mail(mail):
+    result = await dbcol_mails.insert_one(mail)
+    print('result %s' % repr(result.inserted_id))
